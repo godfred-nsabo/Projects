94305bf51a8cf318f4c79e84ccc4a7b2048b2541
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2020-10-08 18:55:24 +0000

    Add files via upload

--- Build a Quiz App Using HTML, CSS and JavaScript/11 Fetch API to Load Questions from Local JSON File/game.js
+++ Build a Quiz App Using HTML, CSS and JavaScript/11 Fetch API to Load Questions from Local JSON File/game.js
@@ -11,7 +11,6 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-
 /*if (!("scramble" in Array.prototype)) {
     Object.defineProperty(Array.prototype, "scramble", {
         enumerable: false,
@@ -28,19 +27,100 @@ let availableQuestions = [];
     });
 };*/
 //function shuffle () {
-    
+
 let questions = [];
-
-fetch("Question.json").then(response => {
-    //console.log(response);
-    return response.json();
-}).then(loadedQuestions => {
-    console.log(loadedQuestions);
+fetch("questions.json").then( (res) => {
+    console.log(res);
+    return res.json();
+}).then( (loadedQuestions) => {
+    console.log( loadedQuestions);
     questions = loadedQuestions;
-
     startGame();
 });
 
+    /*{
+        Question: "Who invented JavaScript?",
+        choice1: "Douglas Crockford",
+        choice2: "Sheryl Sandberg",
+        choice3: "Brendan Eich",
+        choice4: "Steve Jobs",
+        answer: 3
+    },
+    {
+        Question: "Which one of these is a JavaScript package manager?",
+        choice1: "Node.js",
+        choice2: "TypeScript",
+        choice3: "npm",
+        choice4: "Maven",
+        answer: 3
+    },
+    {
+        Question: "Which tool can you use to ensure code quality?",
+        choice1: "Angular",
+        choice2: "jQuery",
+        choice3: "RequireJS",
+        choice4: "ESLint",
+        answer: 4
+
+    },
+    {
+        Question: 'Inside which HTML element do we put the JavaScript??',
+        choice1: '<script>',
+        choice2: '<javascript>',
+        choice3: '<js>',
+        choice4: '<scripting>',
+        answer: 1,
+    },
+    {
+        Question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choice1: "<script href='xxx.js'>",
+        choice2: "<script name='xxx.js'>",
+        choice3: "<script src='xxx.js'>",
+        choice4: "<script file='xxx.js'>",
+        answer: 3,
+    },
+    {
+        Question: " How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World');",
+        choice3: "msg('Hello World');",
+        choice4: "alert('Hello World');",
+        answer: 4,
+    },
+
+    {
+        Question: "What is the full form of IP?",
+        choice1: "Internet Provider",
+        choice2: "Internet Port",
+        choice3: "Internet Protocol",
+        choice4: "Other",
+        answer: 4
+    },
+    {
+        Question: "Who is the founder of Microsoft?",
+        choice1: "Bill Gates",
+        choice2: "Steve Jobs",
+        choice3: "Steve Wozniak",
+        choice4: "Martin Shaba",
+        answer: 1
+    },
+    {
+        Question: "What was your first dream?",
+        choice1: "8 bits",
+        choice2: "64 bits",
+        choice3: "1024 bits",
+        choice4: "16 bits",
+        answer: 1
+    },
+    {
+        Question: "What does CC mean in emails?",
+        choice1: "Carbon Copy",
+        choice2: "Creative Commons",
+        choice3: "Copy Carbon",
+        choice4: "Create Copy",
+        answer: 1
+    }
+];
 /*questions.forEach(q => q.choices.scramble());
 //console.log(questions.choices);
 for (var setIndex = 0; setIndex < questionSets.length; ++setIndex) {
