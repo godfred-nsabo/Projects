5b280718cd0286b63735b41f294370584f0022f3
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2020-10-09 12:15:02 +0000

    Add files via upload

--- Build a Quiz App Using HTML, CSS and JavaScript/8 Create and Style the End Page/end.js
+++ Build a Quiz App Using HTML, CSS and JavaScript/8 Create and Style the End Page/end.js
@@ -3,16 +3,8 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 //const button = document.querySelector ('button');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-const MAX_HIGH_SCORES = 5;
-
-    //console.log("highScores");
-    
-// localStorage.setItem("highScores", JSON.stringify([]));
-//console.log(JSON.parse(localStorage.getItem("highScores")));
-
 finalScore.innerText = mostRecentScore;
+
 
 Username.addEventListener( 'keypup', () => {
     console.log(Username.value);
@@ -22,20 +14,4 @@ saveHighScore = e => {
     console.log("clicked the save button!");
     e.preventDefualt();
     
-	const score = {
-        score: Math.floor( Math.random() * 100),
-		//score: mostRecentScore,
-		name: Username.value
-    };
-    highScores.push(score);
-    // highScores.sort((a, b) => a.score - b.score)
-    highScores.sort( (x, y) => {
-        return y.score - x.score;
-    })
-    highScore.splice(5);
-
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign("/");
-    
-    //console.log(highScores);
 };
