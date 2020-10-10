dbe499b1f72385d14619059cab874028d997e217
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2020-10-10 01:01:42 +0000

    updated man.js
    Writing HTML and document in the .js file

--- Complete jQuery and AJAX/js/main.js
+++ Complete jQuery and AJAX/js/main.js
@@ -1,5 +1,9 @@
 //console.log("Hello World!");
 
 $(document).ready(function(){
-  console.log("We are ready!");
+  $('body').text('Hello World!'); // '' working as a selector. we work with body tag in the .js
+  $('body').html('<strong> Hello World!</strong>')
+  //console.log("We are ready!");
+  //console.log(document);
+  //console.log($(document));
 });
