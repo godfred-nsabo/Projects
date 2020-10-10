6ba673f5cb6b23350a8226b9a16aefb3eca87dab
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2020-10-10 02:21:57 +0000

    Selecting Inputs
    Here we created and highlighted an inputs

--- Complete jQuery and AJAX/js/main.js
+++ Complete jQuery and AJAX/js/main.js
@@ -1,6 +1,9 @@
 //console.log("Hello World!");
 
 $(document).ready(function(){
+
+  //Previous Lesson
+
   //$('body').text('Hello World!'); // '' working as a selector. we work with body tag in the .js
   //$('body').html('<strong> Hello World!</strong>')
   //console.log("We are ready!");
@@ -10,6 +13,22 @@ $(document).ready(function(){
   //$('#container >> .solid').addClass('highlight');
   //$('#container > .non-solid').addClass('highlight'); // > for immediate children
   // and >> for higher (2nd) levels
-  $('#container >>> .non-solid').addClass('highlight');
+  //$('#container >>> .non-solid').addClass('highlight');
+
+  // New Lesson
+
+  //$('#container input').addClass('highlight');
+  //$('#container input:required').addClass('highlight');
+  //$('#container input[placeholder=E-mail]').addClass('highlight');
+  // placeholder[]: Elements in the parenthesis is called attributes
+
+  // for highlighting only Last Name.
+  //$('#container input[placeholder="Last Name"]').addClass('highlight'); // space between text should be in quotes
+  // *= for highlighting first and beneath
+
+  $('#container input[placeholder*="First Name"]').addClass('highlight');
+
+
+
 
 });
