3a5f7081d917013768906b8a381ef649113e86a4
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2020-10-10 01:24:19 +0000

    Selecting by ID
    Highlighting the text in HTML without writing too much CSS

--- Complete jQuery and AJAX/js/main.js
+++ Complete jQuery and AJAX/js/main.js
@@ -1,9 +1,10 @@
 //console.log("Hello World!");
 
 $(document).ready(function(){
-  $('body').text('Hello World!'); // '' working as a selector. we work with body tag in the .js
-  $('body').html('<strong> Hello World!</strong>')
+  //$('body').text('Hello World!'); // '' working as a selector. we work with body tag in the .js
+  //$('body').html('<strong> Hello World!</strong>')
   //console.log("We are ready!");
   //console.log(document);
   //console.log($(document));
+  $('#fire').addClass('highlight');
 });
